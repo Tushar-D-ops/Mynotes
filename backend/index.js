c86d792d835cad8d57b362f5f1cfe86281855a36
Tushar-D-ops <tushardhakate6@gmail.com>
2025-05-24 16:44:31 +0530

    awda

--- backend/index.js
+++ backend/index.js
@@ -155,7 +155,7 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin:["http://localhost:5173"],credentials:true}))
+app.use(cors({ origin:["https://mynotes-omega-six.vercel.app/"],credentials:true}))
 app.use("/api/auth", authRouter)
 app.use("/api/note", authenticateToken,noteRouter)
 
